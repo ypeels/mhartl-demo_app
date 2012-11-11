5e225a9f5df633410abee805c2052a5d7478606d
Jonathan Chen <ypeels@live.com>
2012-11-10 21:25:18 -0600

    Section 2.3.2 Putting the micro in microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  
+  # Section 2.3.2 Listing 2.9 - Putting the micro in microposts
+  validates :content, :length => { : maximum => 140 }
+  
 end
