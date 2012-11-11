6a63df2ee17dd8eea2d3d61b6178c8ea16b90ebf
Jonathan Chen <ypeels@live.com>
2012-11-10 21:27:21 -0600

    Section 2.3.3 user has_many microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,7 @@ class Micropost < ActiveRecord::Base
   # Section 2.3.2 Listing 2.9 - Putting the micro in microposts
   validates :content, :length => { : maximum => 140 }
   
+  # Section 2.3.3 Listing 2.11
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  
+  # Section 2.3.3 Listing 2.10
+  has_many :microposts
 end
